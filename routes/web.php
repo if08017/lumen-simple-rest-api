3fcba1afed36936d41f4ab755b3d2989fafe718b
if08017@gmail.com <if08017@gmail.com>
2018-10-15 17:15:43 +0700

    chore(Router): add login, register and get user route

--- routes/web.php
+++ routes/web.php
@@ -12,7 +12,10 @@
 */
 
 $router->get('/', function () use ($router) {
-    return $router->app->version();
+  $res['version'] = $router->app->version();
+  $res['success'] = true;
+  $res['result'] = "Hello, welcome to lumen";
+  return response()->json($res, 201);
 });
 $router->group(['prefix' => 'api', 'middleware' => 'auth'], function() use ($router){
   $router->get('/posts', ['uses' => 'PostController@index']);
@@ -21,3 +24,7 @@ $router->group(['prefix' => 'api', 'middleware' => 'auth'], function() use ($rou
   $router->put('/posts/{id}', 'PostController@update');
   $router->delete('/posts/{id}', 'PostController@delete');
 });
+
+$router->post('/login', 'LoginController@index');
+$router->post('/register', 'UserController@register');
+$router->get('/user/{id}', ['middleware' => 'auth', 'uses' =>  'UserController@getUser']);
